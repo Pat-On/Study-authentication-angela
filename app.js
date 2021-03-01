d587079e93aca609297382f16b2b7df395975720
PatOn <mahomed2278@gmail.com>
2021-03-01 09:24:29 +0000

    level 3

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -33,10 +34,10 @@ const userSchema = new mongoose.Schema({
 
 
 //plugin encryption and the secret word - needed to be done before mongoose.model()
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"] //possible - anceyption of many fields
-})
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ["password"] //possible - anceyption of many fields
+// })
 
 
 // setting new user model
@@ -56,7 +57,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   })
 
   newUser.save(function(err) {
@@ -70,7 +71,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({
     email: username
   }, function(err, foundUser) {
