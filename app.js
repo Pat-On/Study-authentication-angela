450941853eb5685dbd0b1c1665670b9248134cff
PatOn <mahomed2278@gmail.com>
2021-03-01 06:43:31 +0000

    dotenv added

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
+const encrypt = require("mongoose-encryption");
+
 const app = express();
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -15,11 +20,24 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-//schema creation
-const userSchema = {
+//schema creation - it is just normal JS object
+// const userSchema = {
+//   email: String,
+//   password: String
+// };
+//Upgrade: creating proper Schema base on mongoose - objec t created from mongoose.schema class
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+
+//plugin encryption and the secret word - needed to be done before mongoose.model()
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"] //possible - anceyption of many fields
+})
+
 
 // setting new user model
 const User = new mongoose.model("User", userSchema);
@@ -38,7 +56,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    Password: req.body.password,
+    password: req.body.password,
   })
 
   newUser.save(function(err) {
@@ -50,7 +68,7 @@ app.post("/register", function(req, res) {
   })
 })
 
-app.post("/login", function() {
+app.post("/login", function(req, res) {
   const username = req.body.username;
   const password = req.body.password;
   User.findOne({
@@ -60,6 +78,7 @@ app.post("/login", function() {
       console.log(err)
     } else {
       if (foundUser) {
+        console.log(foundUser.password)
         if (foundUser.password === password) {
           res.render("secrets")
         }
